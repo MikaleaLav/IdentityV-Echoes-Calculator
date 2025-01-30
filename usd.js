b7f73a008fdb44411b7be278280f40325f0160d9
aseelhijazi <aseelhijazi707@gmail.com>
2025-01-30 14:42:29 +0300

    add more images

--- usd.js
+++ usd.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Array of background image URLs
     const backgroundImages = [
-        'https://i.redd.it/pt6tuw5ijrna1.jpg',
+        'https://pbs.twimg.com/media/Ge1RiR8bkAAbqE4?format=jpg&name=4096x4096',
+        'https://pbs.twimg.com/media/GhKUOKVbIAAc_F5?format=jpg&name=4096x4096',
         'https://pbs.twimg.com/media/EGWQsgmUUAAJ_cq?format=jpg&name=4096x4096',
         'https://pbs.twimg.com/media/GD2SWamagAA9iNd?format=jpg&name=4096x4096',
         'https://pbs.twimg.com/media/F6hD7EcbQAA2vOX?format=jpg&name=large',
@@ -19,14 +20,11 @@ document.addEventListener('DOMContentLoaded', function () {
         'https://pbs.twimg.com/media/GRU-0d_XwAAlODV?format=jpg&name=large',
     ];
 
-    // Function to select a random image from the array
     function getRandomImage(images) {
         return images[Math.floor(Math.random() * images.length)];
     }
 
-    // Set the random background image
-    const randomImage = getRandomImage(backgroundImages);
-    body.style.backgroundImage = `url('${randomImage}')`;
+    body.style.backgroundImage = `url('${getRandomImage(backgroundImages)}')`;
 
     form.addEventListener('submit', function (event) {
         event.preventDefault();
@@ -42,17 +40,16 @@ document.addEventListener('DOMContentLoaded', function () {
         const quantities = new Array(8).fill(0);
 
         const packages = [
-            { echoes: 60, bonus: 60, price: 0.99 },
-            { echoes: 185, bonus: 194, price: 2.99 },
-            { echoes: 305, bonus: 320, price: 4.99 },
-            { echoes: 690, bonus: 723, price: 9.99 },
-            { echoes: 1308, bonus: 1371, price: 19.99 },
-            { echoes: 2025, bonus: 2123, price: 29.99 },
-            { echoes: 3330, bonus: 3498, price: 49.99 },
-            { echoes: 6590, bonus: 6918, price: 99.99 },
+            { bonus: 60, price: 0.99 },
+            { bonus: 194, price: 2.99 },
+            { bonus: 320, price: 4.99 },
+            { bonus: 723, price: 9.99 },
+            { bonus: 1371, price: 19.99 },
+            { bonus: 2123, price: 29.99 },
+            { bonus: 3498, price: 49.99 },
+            { bonus: 6918, price: 99.99 },
         ];
 
-        // Iterate over the packages from the largest to the smallest
         for (let i = packages.length - 1; i >= 0; i--) {
             while (remainingEchoes >= packages[i].bonus) {
                 remainingEchoes -= packages[i].bonus;
@@ -61,26 +58,20 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
 
-        // Handle remaining echoes if not perfectly divisible
         if (remainingEchoes > 0) {
             for (let i = 0; i < packages.length; i++) {
                 if (remainingEchoes <= packages[i].bonus) {
                     totalCostUSD += packages[i].price;
                     quantities[i]++;
-                    remainingEchoes = 0; // All echoes are now covered
+                    remainingEchoes = 0;
                     break;
                 }
             }
         }
 
-        resultDiv.innerHTML = `
-            <div class="total-cost-box">
-                Total Cost: $${totalCostUSD.toFixed(2)} (USD)
-            </div>
-        `;
+        resultDiv.innerHTML = `<div class="total-cost-box">Total Cost: $${totalCostUSD.toFixed(2)} (USD)</div>`;
 
-        const quantityCells = document.querySelectorAll('.quantity');
-        quantityCells.forEach((cell, index) => {
+        document.querySelectorAll('.quantity').forEach((cell, index) => {
             cell.textContent = quantities[index] > 0 ? quantities[index] : '-';
         });
     });
